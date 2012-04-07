e51a82a11330a6436ab94f2ae09a5587a567c0ec
Anil Kulkarni <me@wngz3r0.info>
2012-04-07 01:05:57 -0700

    Added NewCircle fonts

---
+++ header.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	</head>
+	<body>
+		<p style="font-family:NewCircle">Regular</p>
+		<p style="font-family:NewCircleSemiBold">SemiBold</p>
+		<p style="font-family:NewCircleBold">Bold</p>
+		<p style="font-family:NewCircleGordita">Gordita</p>
+		<p style="font-family:NewCircleGorditaItalic">GorditaItalic</p>
+	</body>
---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<?php
+/**
+ * @package WordPress
+ * @subpackage metro
+ */
+
+get_header(); ?>
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		hi
+		<?php get_template_part( 'content', get_post_format() ); ?>
+	<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
