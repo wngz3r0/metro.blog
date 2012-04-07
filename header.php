38573cc3840b662fe75dd49d5d657d25c20a1336
Anil Kulkarni <me@wngz3r0.info>
2012-04-07 06:10:17 -0700

    Simplifying font story to just encompass modern browsers using WOFF. I tested this on IE9, FF, and Chrome. The only requirement is that IE9 renders the site in standards mode, hence the meta tag. Since there is no bold font, but New Cicle Gordita is a darker typset, I'm using that as the bold font

--- header.php
+++ header.php
@@ -1,11 +1,15 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="X-UA-Compatible" content="edge" />
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	</head>
 	<body>
-		<h1 style="font-family:NewCicleFina">Regular</p>
-		<h1 style="font-family:NewCicleSemi">SemiBold</p>
-		<h1 style="font-family:NewCicleGordita">Gordita</p>
-		<h1 style="font-family:NewCicleGorditaItalic">GorditaItalic</p>
+		<h1>Lorem ipsum dolor sit amet<h1>
+		<h2>consectetur adipiscing elit. Sed consequat, elit et aliquam tincidunt, mauris felis faucibus nulla, vitae egestas turpis enim eget massa. Integer in nisi ipsum, ac ullamcorper lorem. Morbi sit amet mauris sapien, quis consequat libero. Etiam ipsum sapien, sollicitudin quis gravida id, laoreet in tellus. Praesent erat mi, rutrum quis cursus ac, venenatis sed lacus. Morbi vehicula condimentum gravida. Suspendisse volutpat ultrices hendrerit. Ut ante mauris, rutrum sed imperdiet ut, elementum at erat. Maecenas pellentesque ultricies elit nec consectetur. Pellentesque arcu orci, pellentesque tempor ornare at, tristique sit amet elit. Suspendisse potenti. Proin vestibulum sem sed sem gravida quis facilisis enim placerat. Morbi et erat arcu. Proin bibendum vestibulum purus, at sodales urna tempor eu. Sed cursus, nisi ac accumsan ullamcorper, ante nibh mattis mi, et ornare risus metus id ante.<h2>
+ 
+<h3>Phasellus mi eros,</h3> <p>scelerisque at malesuada eu, vestibulum id est. Cras lectus turpis, auctor sit amet auctor sit amet, suscipit ut tortor. Etiam aliquet sodales ipsum ac lobortis. Ut imperdiet facilisis urna at ullamcorper. Nam varius hendrerit consectetur. Aliquam ut ipsum et libero rutrum gravida. Nam sed augue nec ipsum commodo viverra. Morbi congue, quam in tristique adipiscing, dui diam lacinia leo, ut molestie ante nulla sed arcu. Pellentesque sollicitudin urna eu nibh condimentum nec consequat nisl ornare. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer sem sem, rhoncus sit amet condimentum non, luctus ac enim. Fusce eu nisi id ipsum lacinia iaculis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus ante, faucibus id aliquet et, laoreet malesuada ipsum. In hac habitasse platea dictumst.
+ 
+Etiam vel tellus vitae odio mollis rhoncus interdum ut augue. Mauris ultrices sem a ante sollicitudin id pellentesque dolor auctor. Donec non tellus eu nunc dapibus pulvinar. Sed ac dolor enim. Phasellus nec felis quam, ac lacinia justo. Integer consequat libero ac erat fringilla congue. Integer sit amet erat quis massa porttitor scelerisque eu eget erat. Praesent ac tristique arcu. Vivamus fermentum diam mauris. Nam ac nisl non turpis iaculis dapibus. Curabitur condimentum posuere rutrum.</p>
+ 
 	</body>
